faf6127ee506aa91c4fe3e267c1ed877a5c81e88
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-08-23 12:15:10 +0200

    Update RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver.ino

--- RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver/RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver.ino
+++ RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver/RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver.ino
@@ -28,12 +28,12 @@
 #define inRxBat  A7
 
 //pins for nRF24L01
-#define CE       A4 
-#define CSN      A3 
+#define CE       A0 
+#define CSN      A1 
 
 //software SPI http://tmrh20.github.io/RF24/Arduino.html
-//----- MISO     14 - A0
-//----- MOSI     15 - A1
+//----- MISO     18 - A4
+//----- MOSI     17 - A3
 //----- SCK      16 - A2
 
 RF24 radio(CE, CSN); //setup CE and CSN pins
