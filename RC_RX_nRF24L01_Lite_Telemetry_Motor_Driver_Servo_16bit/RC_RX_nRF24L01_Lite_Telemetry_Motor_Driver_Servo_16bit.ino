05b804cfc6da1f1f3714b2399ec6b77bd3ecdcf3
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-09-11 02:19:52 +0200

    Update RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver_Servo_16bit.ino

--- RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver_Servo_16bit/RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver_Servo_16bit.ino
+++ RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver_Servo_16bit/RC_RX_nRF24L01_Lite_Telemetry_Motor_Driver_Servo_16bit.ino
@@ -196,10 +196,10 @@ void setup()
   pinMode(pwm3, OUTPUT);
   pinMode(pwm4, OUTPUT);
   
-  pinMode(led, OUTPUT);    //LED RX battery and RF on/off
-  pinMode(inRxBat, INPUT); //input RX battery
+  pinMode(led, OUTPUT);
+  pinMode(inRxBat, INPUT);
   
-  resetData(); //reset each channel value
+  resetData();
   attachServoPins();
 
   //define the radio communication
@@ -235,7 +235,7 @@ void loop()
 } //end program loop
 
 //************************************************************************************************************************************************************************
-//get time after losing RF data or turning off the TX, reset data and the LED activates flashing  ************************************************************************
+//get time after losing RF data or turning off the TX, reset data and the LED flashing ***********************************************************************************
 //************************************************************************************************************************************************************************
 unsigned long lastReceiveTime = 0;
 
