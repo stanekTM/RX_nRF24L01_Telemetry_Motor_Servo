02c50c81812f5f93d5c563acf50023e073a6fe1e
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-10-22 01:45:09 +0200

    Update RC_RX_nRF24L01_Telemetry_Motor_Driver_Servo_8bit.ino

--- RC_RX_nRF24L01_Telemetry_Motor_Driver_Servo_8bit/RC_RX_nRF24L01_Telemetry_Motor_Driver_Servo_8bit.ino
+++ RC_RX_nRF24L01_Telemetry_Motor_Driver_Servo_8bit/RC_RX_nRF24L01_Telemetry_Motor_Driver_Servo_8bit.ino
@@ -6,6 +6,10 @@
 #include "ServoTimer2.h"  //used locally https://github.com/nabontra/ServoTimer2
 #include "PWMFrequency.h" //used locally https://github.com/TheDIYGuy999/PWMFrequency
 
+// RX battery voltage settings
+#define RX_battery_voltage   4.2
+#define RX_monitored_voltage 3.3
+
 //free pins
 //pin            3
 //pin            4
@@ -253,7 +257,7 @@ void receive_time()
   if(millis() >= lastRxTime + 1000) //1000 (1second)
   {
     resetData();       
-    RFoff_indication(); 
+    RFoff_check(); 
   }
 }
 
@@ -271,7 +275,7 @@ void send_and_receive_data()
     radio.read(&rc_data, sizeof(packet));
     
     lastRxTime = millis(); //at this moment we have received the data
-    RXbatt_indication();                     
+    RX_batt_check();                     
   } 
 }
 
@@ -281,8 +285,7 @@ void send_and_receive_data()
 //************************************************************************************************************************************************************************
 void battery_voltage()
 { 
-  //-------------------------------------- RX battery -
-  payload.RXbatt = analogRead(inRXbatt) * (4.2 / 1023);
+  payload.RXbatt = analogRead(inRXbatt) * (RX_battery_voltage / 1023);
 
 //  Serial.println(payload.RXbatt); //print value ​​on a serial monitor   
 }
@@ -291,10 +294,9 @@ void battery_voltage()
 unsigned long ledTime = 0;
 int ledState, detect;
 
-void RXbatt_indication()
+void RX_batt_check()
 { 
-  //------------------------ monitored voltage
-  detect = payload.RXbatt <= 3.3;
+  detect = payload.RXbatt <= RX_monitored_voltage;
   
   if (millis() >= ledTime + 500) //1000 (1second)
   {
@@ -315,7 +317,7 @@ void RXbatt_indication()
 //************************************************************************************************************************************************************************
 //when RX is switched on and TX is switched off, or after the loss of RF data = LED RX flash at a interval of 100 ms. Normal mode = LED RX is lit ************************
 //************************************************************************************************************************************************************************
-void RFoff_indication()
+void RFoff_check()
 {
   if (millis() >= ledTime + 100) //1000 (1second)
   {
