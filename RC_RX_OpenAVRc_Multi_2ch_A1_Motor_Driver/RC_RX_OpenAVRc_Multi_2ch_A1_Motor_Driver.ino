7c339425aba4b3e5068d40ec0273aabd75fe61c5
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-11-30 20:12:42 +0100

    Update RC_RX_OpenAVRc_Multi_2ch_A1_Motor_Driver.ino

--- RC_RX_OpenAVRc_Multi_2ch_A1_Motor_Driver/RC_RX_OpenAVRc_Multi_2ch_A1_Motor_Driver.ino
+++ RC_RX_OpenAVRc_Multi_2ch_A1_Motor_Driver/RC_RX_OpenAVRc_Multi_2ch_A1_Motor_Driver.ino
@@ -12,11 +12,11 @@
 #include "PWMFrequency.h" //used locally https://github.com/TheDIYGuy999/PWMFrequency
 
 
-//MotorA PWM frequency pin D9 or pin D10
+//settings PWM MotorA (pin D9 or D10)
 //1024 = 30Hz, 256 = 122Hz, 64 = 488Hz(default), 8 = 3906Hz 
 #define pwm_motorA 256
 
-//MotorB PWM frequency pin D3 or pin D11
+//settings PWM MotorB (pin D3 or D11)
 //1024 = 30Hz, 256 = 122Hz, 128 = 244Hz, 64 = 488Hz(default), 32 = 976Hz, 8 = 3906Hz  
 #define pwm_motorB 256
 
@@ -111,7 +111,7 @@ void resetData()
 //************************************************************************************************************************************************************************
 //setup frequencies and motors control ***********************************************************************************************************************************
 //************************************************************************************************************************************************************************
-int motA_value = 0, motB_value = 0;
+int value_motorA = 0, value_motorB = 0;
 
 void outputPWM()
 {  
@@ -147,14 +147,14 @@ void outputPWM()
 
   if (rc_data.ch1 < servoMid - dead_zone)
   {
-    motA_value = map(rc_data.ch1, servoMid - dead_zone, servoMin, 0, 255);
-    analogWrite(pin_pwm1_motorA, motA_value); 
+    value_motorA = map(rc_data.ch1, servoMid - dead_zone, servoMin, 0, 255);
+    analogWrite(pin_pwm1_motorA, value_motorA); 
     digitalWrite(pin_pwm2_motorA, LOW);
   }
   else if (rc_data.ch1 > servoMid + dead_zone)
   {
-    motA_value = map(rc_data.ch1, servoMid + dead_zone, servoMax, 0, 255);
-    analogWrite(pin_pwm2_motorA, motA_value); 
+    value_motorA = map(rc_data.ch1, servoMid + dead_zone, servoMax, 0, 255);
+    analogWrite(pin_pwm2_motorA, value_motorA); 
     digitalWrite(pin_pwm1_motorA, LOW);
   }
   else
@@ -169,14 +169,14 @@ void outputPWM()
 
   if (rc_data.ch2 < servoMid - dead_zone)
   {
-    motB_value = map(rc_data.ch2, servoMid - dead_zone, servoMin, 0, 255); 
-    analogWrite(pin_pwm3_motorB, motB_value); 
+    value_motorB = map(rc_data.ch2, servoMid - dead_zone, servoMin, 0, 255); 
+    analogWrite(pin_pwm3_motorB, value_motorB); 
     digitalWrite(pin_pwm4_motorB, LOW);
   }
   else if (rc_data.ch2 > servoMid + dead_zone)
   {
-    motB_value = map(rc_data.ch2, servoMid + dead_zone, servoMax, 0, 255); 
-    analogWrite(pin_pwm4_motorB, motB_value); 
+    value_motorB = map(rc_data.ch2, servoMid + dead_zone, servoMax, 0, 255); 
+    analogWrite(pin_pwm4_motorB, value_motorB); 
     digitalWrite(pin_pwm3_motorB, LOW);
   }
   else
@@ -276,7 +276,7 @@ void send_and_receive_data()
 
 //************************************************************************************************************************************************************************
 //measuring the input of the RX battery. After receiving RF data, the monitored RX battery is activated ******************************************************************
-//RX battery_voltage < monitored_voltage = LED alarm RX flash at a interval of 0.5s. Battery OK = LED RX is lit **********************************************************
+//when RX battery_voltage < monitored_voltage = LED alarm RX flash at a interval of 0.5s. Battery OK = LED RX is lit *****************************************************
 //************************************************************************************************************************************************************************
 unsigned long ledTime = 0;
 int ledState, detect;
