0e8b06e901ce964f5ab6d153e1f6008decbf0d92
stanekTM <44894972+stanekTM@users.noreply.github.com>
2020-12-02 02:06:10 +0100

    Update RC_RX_nRF24L01_2ch_A1_Motor_Driver.ino

--- RC_RX_nRF24L01_2ch_A1_Motor_Driver/RC_RX_nRF24L01_2ch_A1_Motor_Driver.ino
+++ RC_RX_nRF24L01_2ch_A1_Motor_Driver/RC_RX_nRF24L01_2ch_A1_Motor_Driver.ino
@@ -252,7 +252,7 @@ unsigned long lastRxTime = 0;
 
 void receive_time()
 {
-  if(millis() >= lastRxTime + 400) //400 = 3.3VCC, 1000 = 5VCC
+  if(millis() >= lastRxTime + 1000) //1s
   {
     resetData();       
     RFoff_check(); 
@@ -290,7 +290,7 @@ void RX_batt_check()
   
   detect = payload.RXbatt <= monitored_voltage;
   
-  if (millis() >= ledTime + 200) //200 = 3.3VCC, 500 = 5VCC
+  if (millis() >= ledTime + 500)
   {
     ledTime = millis();
     
@@ -311,7 +311,7 @@ void RX_batt_check()
 //************************************************************************************************************************************************************************
 void RFoff_check()
 {
-  if (millis() >= ledTime + 30) //30 = 3.3VCC, 100 = 5VCC
+  if (millis() >= ledTime + 100)
   {
     ledTime = millis();
     
