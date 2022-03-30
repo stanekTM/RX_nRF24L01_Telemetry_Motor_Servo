f89141cc6c72f8539146d9e0bd14d6584e5ff494
stanekTM <44894972+stanekTM@users.noreply.github.com>
2022-03-31 00:30:21 +0200

    Update RX_OpenAVRc_Multi_12ch_A1_Servo.ino

--- RX_OpenAVRc_Multi_12ch_A1_Servo/RX_OpenAVRc_Multi_12ch_A1_Servo.ino
+++ RX_OpenAVRc_Multi_12ch_A1_Servo/RX_OpenAVRc_Multi_12ch_A1_Servo.ino
@@ -27,38 +27,38 @@ const byte address[] = "jirka";
 #define max_control_val 2000
 
 //free pins
-//pin                0
-//pin                1
-//pin                A6
+//pin                  0
+//pin                  1
+//pin                  A6
 
 //pins for servos
-#define pin_servo1   2
-#define pin_servo2   3
-#define pin_servo3   4
-#define pin_servo4   5
-#define pin_servo5   6
-#define pin_servo6   7
-#define pin_servo7   8
-#define pin_servo8   9
-#define pin_servo9   10
-#define pin_servo10  11 //MOSI
-#define pin_servo11  12 //MISO
-#define pin_servo12  13 //SCK
+#define pin_servo1     2
+#define pin_servo2     3
+#define pin_servo3     4
+#define pin_servo4     5
+#define pin_servo5     6
+#define pin_servo6     7
+#define pin_servo7     8
+#define pin_servo8     9
+#define pin_servo9     10
+#define pin_servo10    11 //MOSI
+#define pin_servo11    12 //MISO
+#define pin_servo12    13 //SCK
 
 //LED RX battery and RF on/off
-#define pin_LED      A5
+#define pin_LED        A5
 
 //input RX battery
-#define pin_RXbatt   A7
+#define pin_RX_battery A7
 
 //pins for nRF24L01
-#define pin_CE       A0
-#define pin_CSN      A1
+#define pin_CE         A0
+#define pin_CSN        A1
 
 //software SPI http://tmrh20.github.io/RF24/Arduino.html
-//----- SCK     16 - A2
-//----- MOSI    17 - A3
-//----- MISO    18 - A4
+//----- SCK       16 - A2
+//----- MOSI      17 - A3
+//----- MISO      18 - A4
 
 //setting of CE and CSN pins
 RF24 radio(pin_CE, pin_CSN);
@@ -179,7 +179,7 @@ void setup()
 //  printf_begin();     //print the radio debug info
 
   pinMode(pin_LED, OUTPUT);
-  pinMode(pin_RXbatt, INPUT);
+  pinMode(pin_RX_battery, INPUT);
 
   fail_safe();
   attachServoPins();
@@ -227,7 +227,7 @@ void receive_time()
   if(millis() >= lastRxTime + 1000) //1s
   {
     fail_safe();
-    RFoff_check();
+    RF_off_check();
   }
 }
 
@@ -262,7 +262,7 @@ void RX_batt_check()
   {
     adcTime = millis();
     
-    telemetry_packet.RX_batt_A1 = map(analogRead(pin_RXbatt), 0, 1023, 0, 255);
+    telemetry_packet.RX_batt_A1 = map(analogRead(pin_RX_battery), 0, 1023, 0, 255);
   }
   
   detect = telemetry_packet.RX_batt_A1 <= (255 / battery_voltage) * monitored_voltage;
@@ -287,7 +287,7 @@ void RX_batt_check()
 //************************************************************************************************************************************************************************
 //when RX is switched on and TX is switched off, or after the loss of RF data = LED RX flash at a interval of 0.1s. Normal mode = LED RX is lit **************************
 //************************************************************************************************************************************************************************
-void RFoff_check()
+void RF_off_check()
 {
   if (millis() >= ledTime + 100)
   {
