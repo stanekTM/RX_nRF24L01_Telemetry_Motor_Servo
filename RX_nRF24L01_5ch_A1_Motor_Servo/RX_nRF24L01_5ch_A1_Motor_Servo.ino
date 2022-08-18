e8604486d263dfd2cd36f036f8d814993cc27b2c
stanekTM <44894972+stanekTM@users.noreply.github.com>
2022-08-18 22:34:52 +0200

    Update RX_nRF24L01_5ch_A1_Motor_Servo.ino

--- RX_nRF24L01_5ch_A1_Motor_Servo/RX_nRF24L01_5ch_A1_Motor_Servo.ino
+++ RX_nRF24L01_5ch_A1_Motor_Servo/RX_nRF24L01_5ch_A1_Motor_Servo.ino
@@ -34,7 +34,7 @@ const byte address[] = "jirka";
 #define ACCELERATE_MOTOR_A  90
 #define ACCELERATE_MOTOR_B  40
 
-//setting the maximum engine power. Suitable for TX transmitters without endpoint setting (0-255)
+//setting the maximum motor power. Suitable for TX transmitters without endpoint setting (0-255)
 #define MAX_MOTOR_A  255
 #define MAX_MOTOR_B  255
 
