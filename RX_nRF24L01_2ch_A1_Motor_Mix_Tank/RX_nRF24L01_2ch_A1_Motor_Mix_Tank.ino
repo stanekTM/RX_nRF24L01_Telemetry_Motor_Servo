98bc9aff7c7eebdab97e6bb0d783ca155ee90718
stanekTM <44894972+stanekTM@users.noreply.github.com>
2022-08-30 21:34:10 +0200

    edit link

--- RX_nRF24L01_2ch_A1_Motor_Mix_Tank/RX_nRF24L01_2ch_A1_Motor_Mix_Tank.ino
+++ RX_nRF24L01_2ch_A1_Motor_Mix_Tank/RX_nRF24L01_2ch_A1_Motor_Mix_Tank.ino
@@ -1,8 +1,8 @@
 
 //************************************************************************************************************************************************************************
-//Communication nRF24L01P "Stanek". Fixed RF channel, fixed address.
-//Support for OpenAVRc transmitter https://github.com/stanekTM/OpenAVRc_Dev
-//and for the Multiprotocol TX modul https://github.com/stanekTM/DIY-Multiprotocol-TX-Module
+//Communication nRF24L01P. Fixed RF channel, fixed address.
+//Support for TX Telemetry LCD transmitter https://github.com/stanekTM/TX_nRF24L01_4ch_Telemetry_LCD
+//and for the TX Telemetry LED transmitter https://github.com/stanekTM/TX_nRF24L01_5ch_Telemetry_LED
 //************************************************************************************************************************************************************************
 
 #include <RF24.h>         //https://github.com/nRF24/RF24 v1.3.9
